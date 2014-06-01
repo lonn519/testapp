1ba720a667f9160636acc08e7a88f5881f9e17bf
Lonn Lee <lonn@grandtrunklabs.com>
2014-06-01 01:30:03 -0700

    added a few more files in the questions folder

---
+++ client/views/questions/questions_list.js
@@ -0,0 +1,20 @@
+var questionsData = [
+  {
+    title: 'Introducing Telescope',
+    author: 'Sacha Greif',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  },
+  {
+    title: 'Meteor',
+    author: 'Tom Coleman',
+    url: 'http://meteor.com'
+  },
+  {
+    title: 'The Meteor Book',
+    author: 'Tom Coleman',
+    url: 'http://themeteorbook.com'
+  }
+];
+Template.questionsList.helpers({
+  questions: questionsData
+});
